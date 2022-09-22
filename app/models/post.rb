9f2d96c9494cc91f3cd2c7cadbeecd4443be3501
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-09-22 20:46:10 +0500

    updated posts error

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  after_save :update_post_counter
+   validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  after_save :update_posts_counter
 
-  def update_post_counter
-    user.increment!(:post_counter)
+  def update_posts_counter
+    user.increment!(:posts_counter)
   end
 
   def five_most_recent_comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'user_id'
   has_many :comments, foreign_key: 'user_id'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   def last_three_posts
     posts.order(updated_at: :desc).limit(3)
   end
