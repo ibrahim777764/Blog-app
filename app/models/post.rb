39fe9b18ceadae1857fec0b6085493a73ad49189
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-09-22 20:51:12 +0500

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-   validates :title, presence: true, length: { maximum: 250 }
+  validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :update_posts_counter
