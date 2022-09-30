0fc3a9b1e90066042c364573c2d51366ba40fa16
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-10-01 00:06:05 +0500

    Allow Users to add likes to Posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
  # root to: "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
   end
 
 end
